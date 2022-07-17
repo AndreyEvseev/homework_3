f8d0098106f100c07f8f39b76a12b8004a494a4f
Andrey Evseev <a-evseev@yandex.ru>
2022-07-17 19:46:46 +0300

    решение задания 5

--- Task3_5.py
+++ Task3_5.py
@@ -1,2 +1,12 @@
 # Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
 
+def fibonacci_list(k: int) -> list:
+    fib_list = [-1, 1, 0, 1, 1]
+    for i in range(3, k+1):
+        fib_list.append(fib_list[-2] + fib_list[-1])
+        fib_list.insert(0, fib_list[1] - fib_list[0])
+    return fib_list
+
+k = int(input('Введите натуральное число: '))
+fib_list = fibonacci_list(k)
+print(fib_list)
