c9dcb7a5a8064582bb7a80f8778f87712da90288
Andrey Evseev <a-evseev@yandex.ru>
2022-07-17 20:24:36 +0300

    решение задания 4

--- Task3_4.py
+++ Task3_4.py
@@ -1,2 +1,15 @@
 # Напишите программу, которая будет преобразовывать десятичное число в двоичное.
 
+import math
+
+def descimal_to_binary(k: int) -> str:
+    bin_k = [k % 2]
+    while math.floor(k / 2) != 0:
+        k = math.floor(k / 2)
+        bin_k.insert(0, k % 2)
+    bin_k = "".join(map(str, bin_k))
+    return bin_k
+
+k = int(input('Введите натуральное число: '))
+b = descimal_to_binary(k)
+print(f'{k} -> {b}')
